acfd7252b2a1b3bf1d6bbb80fc2d96dabd8f07cb
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-12-04 15:26:30 +0100

    Migrated to numpy for transition probability matrices

--- testing_extraction.py
+++ testing_extraction.py
@@ -1,3 +1,4 @@
+import numpy as np
 import music21
 from music21 import chord, note
 
@@ -8,30 +9,37 @@ def extract_chords(score):
     :param score: score of notes
     """
     chord_stream = score.chordify()
-    chord_list = []
+
+    chords_to_index = {}
+    index = 0
 
     for current_chord in chord_stream.recurse().getElementsByClass(chord.Chord):
         pitches = tuple(sorted(p.midi for p in current_chord.pitches))
-        if pitches not in chord_list:
-            chord_list.append(pitches)
+        if pitches not in chords_to_index:
+            chords_to_index[pitches] = index
+            index += 1
     
-    chord_T_matrix = [[0 for _ in range(len(chord_list))] for _ in range(len(chord_list))]
-    prev_chord = None
+    N = len(list(chords_to_index.keys()))
 
-    for i, current_chord in enumerate(chord_stream.recurse().getElementsByClass(chord.Chord)):
-        if i > 0:
-            x = chord_list.index(tuple(sorted(p.midi for p in prev_chord.pitches)))
-            y = chord_list.index(tuple(sorted(p.midi for p in current_chord.pitches)))
-            chord_T_matrix[x][y] += 1
-        prev_chord = current_chord
+    chord_T_matrix = np.zeros((N, N), dtype=float)
+
+    prev = None
+    for c in chord_stream.recurse().getElementsByClass(chord.Chord):
+        curr = tuple(sorted(p.midi for p in c.pitches))
+        if prev is not None:
+            x = chords_to_index[prev]
+            y = chords_to_index[curr]
+            chord_T_matrix[x, y] += 1
+        prev = curr
     
-    for i, row in enumerate(chord_T_matrix):
-        row_sum = sum(row)
+    for row in chord_T_matrix:
+        row_sum = row.sum()
         if row_sum > 0:
-            for j in range(len(row)):
-                chord_T_matrix[i][j] /= row_sum
+            row /= row_sum
 
-    return chord_T_matrix, chord_list
+    index_to_chords = {i : c for c, i in chords_to_index.items()}
+
+    return chord_T_matrix, chords_to_index, index_to_chords
 
 def extract_notes(score):
     """
@@ -46,40 +54,44 @@ def extract_notes(score):
             melody_part = part
             break
 
-    notes_list = []
+    note_to_index = {}
+    index = 0
 
     for n in melody_part.recurse().getElementsByClass(note.Note):
         note_tuple = tuple([n.nameWithOctave, n.quarterLength])
-
-        if note_tuple not in notes_list:
-            notes_list.append(note_tuple)
+        if note_tuple not in note_to_index:
+            note_to_index[note_tuple] = index
+            index += 1
     
-    note_T_matrix = [[0] * len(notes_list) for _ in range(len(notes_list))]
-    prev_note = None
+    N = len(note_to_index.keys())
 
-    for i, current_note in enumerate(melody_part.recurse().getElementsByClass(note.Note)):
-        if i > 0:
-            x = notes_list.index(tuple([prev_note.nameWithOctave, prev_note.quarterLength]))
-            y = notes_list.index(tuple([current_note.nameWithOctave, current_note.quarterLength]))
-            note_T_matrix[x][y] += 1
-        prev_note = current_note
+    note_T_matrix = np.zeros((N, N), dtype=float)
+
+    prev = None
+    for c in melody_part.recurse().getElementsByClass(note.Note):
+        curr = tuple([c.nameWithOctave, c.quarterLength])
+        if prev is not None:
+            x = note_to_index[prev]
+            y = note_to_index[curr]
+            note_T_matrix[x, y] += 1
+        prev = curr
     
-    for i, row in enumerate(note_T_matrix):
-        row_sum = sum(row)
+    for row in note_T_matrix:
+        row_sum = row.sum()
         if row_sum > 0:
-            for j in range(len(row)):
-                note_T_matrix[i][j] /= row_sum
+            row /= row_sum
     
-    return note_T_matrix, notes_list
+    index_to_note = {i : n for n, i in note_to_index.items()}
+    
+    return note_T_matrix, note_to_index, index_to_note
     
 
 def main():
     dir_path = input('Enter MIDI file path: ')
     score = music21.converter.parse(dir_path)
-    TM, n = extract_notes(score)
+    TM, n = extract_chords(score)
 
-    for row in TM:
-        print(row)
+    print(TM)
 
 if __name__ == '__main__':
     main()
