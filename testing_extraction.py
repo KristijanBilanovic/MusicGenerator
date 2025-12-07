d676071f99b9bd6171293483a1bc682c8e7ed7e6
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2025-12-07 21:23:40 +0100

    Changed chord extraction
    - chord extraction doesn't rely on the .chordify() method anymore
    - chord extraction gives a duration transition matrix alongside other duration properties

--- testing_extraction.py
+++ testing_extraction.py
@@ -1,45 +1,67 @@
 import numpy as np
 import music21
 from music21 import chord, note
+from music21.stream import Score, Part, Opus
 
-def extract_chords(score):
+def extract_chords(score: Score | Part | Opus):
     """
     Return's a list of chords and transition probability matrix
     
     :param score: score of notes
     """
-    chord_stream = score.chordify()
 
     chords_to_index = {}
-    index = 0
+    duration_to_index = {}
 
-    for current_chord in chord_stream.recurse().getElementsByClass(chord.Chord):
+    index_chords = 0
+    index_duration = 0
+
+    # map chords and durations to indices
+    for current_chord in score.recurse().getElementsByClass(chord.Chord):
+        duration = current_chord.duration.quarterLength
         pitches = tuple(sorted(p.midi for p in current_chord.pitches))
         if pitches not in chords_to_index:
-            chords_to_index[pitches] = index
-            index += 1
+            chords_to_index[pitches] = index_chords
+            index_chords += 1
+        if duration not in duration_to_index:
+            duration_to_index[duration] = index_duration
+            index_duration += 1
     
-    N = len(list(chords_to_index.keys()))
+    N_chords = len(list(chords_to_index.keys()))
+    N_durations = len(list(duration_to_index.keys()))
 
-    chord_T_matrix = np.zeros((N, N), dtype=float)
+    chords_TM = np.zeros((N_chords, N_chords), dtype=float)
+    duration_TM = np.zeros((N_durations, N_durations), dtype=float)
 
     prev = None
-    for c in chord_stream.recurse().getElementsByClass(chord.Chord):
+    for c in score.recurse().getElementsByClass(chord.Chord):
         curr = tuple(sorted(p.midi for p in c.pitches))
+        
         if prev is not None:
-            x = chords_to_index[prev]
+            x = chords_to_index[tuple(sorted(p.midi for p in prev.pitches))]
             y = chords_to_index[curr]
-            chord_T_matrix[x, y] += 1
-        prev = curr
+            chords_TM[x, y] += 1
+
+            x = duration_to_index[prev.duration.quarterLength]
+            y = duration_to_index[c.duration.quarterLength]
+            duration_TM[x, y] += 1
+
+        prev = c
     
-    for row in chord_T_matrix:
+    for row in chords_TM:
+        row_sum = row.sum()
+        if row_sum > 0:
+            row /= row_sum
+
+    for row in duration_TM:
         row_sum = row.sum()
         if row_sum > 0:
             row /= row_sum
 
     index_to_chords = {i : c for c, i in chords_to_index.items()}
+    index_to_duration = {i : d for d, i in duration_to_index.items()}
 
-    return chord_T_matrix, chords_to_index, index_to_chords
+    return chords_TM, duration_TM, chords_to_index, index_to_chords, duration_to_index, index_to_duration
 
 def extract_notes(score):
     """
@@ -47,7 +69,7 @@ def extract_notes(score):
     
     :param score: score of notes
     """
-    melody_part = None
+    melody_part: Score = Score()
 
     for part in score.parts:
         if any(n.lyric for n in part.recurse().getElementsByClass(note.Note)):
@@ -97,9 +119,8 @@ def get_score(dir_path):
 def main():
     dir_path = input('Enter MIDI file path: ')
     score = get_score(dir_path)
-    TM, n = extract_chords(score)
+    TM1, TM2, n, k, t, r = extract_chords(score)
 
-    print(TM)
 
 if __name__ == '__main__':
     main()
