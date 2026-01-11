c9fd285c6103976761c4b0d2f01d53dd18cb5e9c
KristijanBilanovic <kristijan.bilanovic@fer.unizg.hr>
2026-01-11 19:55:29 +0100

    Added chord and rest generation

--- MusicGenerator.py
+++ MusicGenerator.py
@@ -16,16 +16,34 @@ class MusicGenerator:
 
         for instr_name in instruments:
             model = self._models_by_instrument[instr_name]
-            music21_instr = music21.instrument.fromString(instr_name)
+            
+            try:
+                music21_instr = music21.instrument.fromString(instr_name)
+            except music21.exceptions21.InstrumentException:
+                music21_instr = music21.instrument.Piano()
 
             generated_sequence = model.generate_sequence(length=length)
             part = music21.stream.Part()
-            for note_name in generated_sequence:
-                note = music21.note.Note(midi=note_name)
-                note.storedInstrument = music21_instr
-                part.append(note)
+            for tm_index in generated_sequence:
+                music_element = MusicDataTrainer.str_to_midi_tuple(tm_index)
+                if len(music_element) > 1: 
+                    c = music21.chord.Chord()
+                    for midi_pitch in music_element:
+                        n = music21.note.Note(midi=midi_pitch)
+                        c.add(n)
+                    c.storedInstrument = music21_instr
+                    part.append(c)
+                    continue
+                elif len(music_element) == 1 and music_element[0] == -1:
+                    r = music21.note.Rest()
+                    part.append(r)
+                    continue
+                elif len(music_element) == 1:
+                    note = music21.note.Note(midi=music_element[0])
+                    note.storedInstrument = music21_instr
+                    part.append(note)
             part.makeMeasures()
             new_score.append(part)
             new_score.makeMeasures()
-            
+
         new_score.show('midi')
